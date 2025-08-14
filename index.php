547334682e108cf9a0db8ab386f116de0f792eeb
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-08-13 21:12:39 -0300

    Improving the appearance of the dashboard
    This time, we made further changes to the appearance of the dashboard to make it a bit more user-friendly. This dashboard will be used to list all records and create new ones. In addition, it will have buttons and other elements to manage the records.

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+	    body{ font-family: "Roboto", sans-serif; }
+	</style>
 	<link rel="stylesheet" type="text/css" href="assets/css/mobile.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/desktop.css">
 </head>
@@ -13,8 +17,11 @@
 	<div class="container">
 
 		<div class="box">
+           
+            <h1 class="supertitle">Web Phonebook</h1>
+
 			<div class="searchengine">
-				<input type="" name="" class="searchengine-input">
+				<input type="" name="" class="searchengine-input" placeholder="Enter something...">
 				<button class="searchengine-button">Search</button>
 			</div>
 
